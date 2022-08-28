977e96008c54ba59e9c02f32be2d71f84fea69fb
six6 <460676145@qq.com>
2022-08-28 12:21:49 +0800

    Signed-off-by: six6 <460676145@qq.com>

--- js/index.js
+++ js/index.js
@@ -2,28 +2,7 @@ $(function(){
     
     $("#index_search_button").click(function(){
         const search = $("#index_search_value").val();
-        queryList(search);
     })
 
 
 })
-const pages = {
-    start:0,
-    size:5
-}
-function queryList(search){
-    $.myAjax({
-        url:"/user/queryList",
-        async:false,
-        data:{
-            search:search,
-            pages:pages
-        },
-        success:function(data){
-            console.log(data);
-        },
-        error:function(e){
-            console.log(e);
-        }
-    })
-}
